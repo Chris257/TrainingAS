b59c2a484e9560cb5370fbbabc66f2fbb7d7a0af
Frank Karlstrøm <fk@databaseforum.no>
2017-03-24 19:21:18 +0100

    Added an initial simple test.

---
+++ src/main/java/com/company/OrderService.java
@@ -0,0 +1,9 @@
+package com.company;
+
+/**
+ * Service class that contains all the business logic for adding and fetching orders.
+ * @author Frank Karlstrøm - frank.karlstrom@gmail.com - fk@databaseforum.no
+ */
+class OrderService {
+    //TODO implement me
+}
---
+++ src/main/java/com/company/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * <p>This application should receive orders for a customer, and process these orders.
+ * <p>How to orders enters the system is not yet defined.
+ * <p>The processing of an order should generate one or more transactions. Storage mechanism is not yet defined.
+ * <p>It should be possible to report on open orders, and report on finished orders. Reporting formats is not yet defined.
+ * @author Frank Karlstrøm - frank.karlstrom@gmail.com - fk@databaseforum.no
+ */
+package com.company;
